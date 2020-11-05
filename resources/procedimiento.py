0f27e002caf3a1a98c63662bf26b49ddee6be7c8
Christian Gutierrez <chcgutierrezm@gmail.com>
2020-11-05 12:59:22 -0500

    Update procedimiento.py
    Se agrega cambio

--- resources/procedimiento.py
+++ resources/procedimiento.py
@@ -1,5 +1,5 @@
 from flask import Response, request
-from database.procedimiento import Paciente
+from database.procedimiento import Procedimiento
 from flask_restful import Resource
 
 class ProcedimientoApi(Resource):
