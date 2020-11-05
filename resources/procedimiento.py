2a7c249b901ca54a06bff478c05453b6e7056c26
Christian Gutierrez <chcgutierrezm@gmail.com>
2020-11-05 12:52:08 -0500

    Update procedimiento.py
    Se

--- resources/procedimiento.py
+++ resources/procedimiento.py
@@ -1,1 +1,14 @@
+from flask import Response, request
+from database.procedimiento import Paciente
+from flask_restful import Resource
 
+class ProcedimientoApi(Resource):
+    def get(self):
+        procedimientos = Procedimiento.objects().to_json()
+        return Response(procedimientos, mimetype="application/json", status=200)
+
+    def post(self):
+        body = request.get_json()
+        procedimiento = Procedimiento(**body).save()
+        id = procedimiento.id
+        return {'id': str(id)}, 200
