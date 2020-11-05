d46e7eca8dee4605a27f382a7a8ecabbb091a516
Christian Gutierrez <chcgutierrezm@gmail.com>
2020-11-05 12:49:14 -0500

    Update routes.py
    Se agrega procedimiento

--- resources/routes.py
+++ resources/routes.py
@@ -1,8 +1,10 @@
 from .client import ClientsApi, ClientApi
 from .paciente import PacienteApi
+from .procedimiento import ProcedimientoApi
 
 def initialize_routes(api):
     api.add_resource(ClientsApi, '/api/clients')
     api.add_resource(ClientApi, '/api/client/<id>')
 
     api.add_resource(PacienteApi, '/api/paciente')
+    api.add_resource(ProcedimientoApi, '/api/procedimiento')
