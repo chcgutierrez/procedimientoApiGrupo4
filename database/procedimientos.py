f582aa8bb3bcc3dc92463247e0b1e871338db037
Christian Gutierrez <chcgutierrezm@gmail.com>
2020-11-05 12:46:51 -0500

    Update procedimientos.py
    Se agregan campos

--- database/procedimientos.py
+++ database/procedimientos.py
@@ -1,1 +1,9 @@
+from .db import db
 
+class Procedimiento(db.Document):
+    procedim_id = db.StringField(required=True, unique=True)
+    nombre = db.StringField(required=True)
+    sexo = db.StringField(required=True)
+    edad = db.ListField(db.StringField(), required=True)
+    finalidad = db.ListField(db.StringField(), required=True)
+    observacion = db.ListField(db.StringField(), required=False)
